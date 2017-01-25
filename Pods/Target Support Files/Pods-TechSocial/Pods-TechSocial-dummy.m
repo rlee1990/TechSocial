da12d423578c4c45e8551fbc70c5903fcc94f3a4
rlee1990 <rickeylee1129@gmail.com>
2017-01-25 18:38:35 -0500

    first commit

---
+++ Pods/Target Support Files/Pods-TechSocial/Pods-TechSocial-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TechSocial : NSObject
+@end
+@implementation PodsDummy_Pods_TechSocial
+@end
---
+++ TechSocial/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  SignInVC.swift
+//  TechSocial
+//
+//  Created by rickey lee on 1/25/17.
+//  Copyright © 2017 rickey lee. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

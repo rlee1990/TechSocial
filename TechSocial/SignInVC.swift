1e48f13704425e4adaa4f1cf52b744b4599f31f8
rlee1990 <rickeylee1129@gmail.com>
2017-01-26 19:57:41 -0500

    New Update
    Completed Facebook and Firebase Auth part 1.

--- TechSocial/SignInVC.swift
+++ TechSocial/SignInVC.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import Firebase
+import FBSDKLoginKit
+import FBSDKCoreKit
 
 class SignInVC: UIViewController {
 
@@ -21,6 +23,32 @@ class SignInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func facebookBtnTapped(_ sender: Any) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+            print("Rickey: Unable to authenticate with Facebook - \(error)")
+            } else if result?.isCancelled == true {
+                print("Rickey: User cancelled Facebook authentication")
+            } else {
+                print("Rickey: Successfully authenticated with Facebook")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        }
+    }
+    
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("Rickey: Unable to authenticate with Firebase - \(error)")
+            } else {
+                print("Rickey: Successfully authenticated with Firebase")
+            }
+        })
+    }
 
 }
 
